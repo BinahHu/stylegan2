eb8df318d34f61280fe222fbae7a9b7620a7148c
胡致远 <2016011260@secoder.net>
2020-02-25 21:38:18 +0800

    update

--- flybitch.sh
+++ flybitch.sh
@@ -1,4 +1,4 @@
 python run_training.py \
-  --num-gpus=4 --data-dir=/datasets --config=config-f \
+  --num-gpus=1 --data-dir=/datasets --config=config-f \
   --dataset=church --content-dataset=conntent --style-dataset=style \
   --total-kimg 1 --gamma=100
