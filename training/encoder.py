b2d879f44c4f531a2a6a4c4dcd5049e6625c115f
胡致远 <2016011260@secoder.net>
2020-02-25 18:01:54 +0800

    test

--- training/encoder.py
+++ training/encoder.py
@@ -12,8 +12,10 @@ def VGG_Encoder(
     image_shape     = [3, 256, 256],            # Input Image shape
     grid_batch      = 480,                      # Batch size
     latent_size     = 512,                      # Latent code size
+    dtype           = 'float32',                # Datatype for output
     **kwargs):
 
     # For test
-    return np.random.randn(grid_batch, latent_size)
+    latent_code_np = np.random.randn(grid_batch, latent_size)
+    return tf.convert_to_tensor(latent_code_np, dtype=dtype)
     return None
